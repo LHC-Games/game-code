1f5a1663a52725785950f01bd0f2d0b8762e0824
Gustavo-Valadares <gustavoh.valadares@outlook.com>
2025-11-19 21:42:53 -0300

    implementação coyote_timer (maior responsividade do pulo resolvida)

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -15,6 +15,8 @@ extends CharacterBody2D
 @onready var right_wall_cast = $WallJump/RightWall
 @onready var left_wall_cast = $WallJump/LeftWall
 
+@export var coyote_duration: float = 0.12
+var coyote_timer: float = 0.0
 
 func _ready() -> void:
 	set_floor_max_angle(0.7)
@@ -25,6 +27,20 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
+	
+#===========================================Coyote Timer=====================================
+	if is_on_floor():
+		coyote_timer = coyote_duration #se tiver no chão reseta o tempo para o máximo
+	
+	else:
+		coyote_timer -= _delta
+		
+	if Input.is_action_just_pressed("up"):
+		if coyote_timer > 0.0:
+			velocity.y = -400
+			coyote_timer = 0.0
+#===============================================================================================
+			
 	move_and_slide()
 	if right_wall_cast.is_colliding():
 		wall_slide.flip_h = true
@@ -38,3 +54,4 @@ func _physics_process(_delta: float) -> void:
 		tmp.x = 18
 		tmp.y = 0
 		wall_slide.position = tmp
+		
